dc583e563fc62e6c69a36d3740853c3be7b1e3d4
elasticnet12345 <elasticnet1995@gmail.com>
2021-06-22 02:46:13 +0900

    fix

--- popup.js
+++ popup.js
@@ -2,12 +2,6 @@
 const showBtnId = 'SHOW';
 const hideBtnId = 'HIDE';
 
-
-// css-1dbjc4n r-1ihkh82 r-1in3vh1 r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1udh08x
-// css-1dbjc4n r-x572qd r-1d6w8o1 r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1udh08
-// css-1dbjc4n r-x572qd r-1d6w8o1 r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1udh08x
-// css-1dbjc4n r-x572qd r-1d6w8o1 r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1udh08x
-
 if (document.getElementById( hideBtnId )){
     document.getElementById( hideBtnId ).addEventListener('click', () => {
         chrome.storage.local.set({'twitterTrendHiderFlag': 'HIDE'}, (property) => {
@@ -32,41 +26,3 @@ if (document.getElementById( showBtnId )){
     });
 }
 
-// if (document.getElementById( hideBtnId )){
-//     document.getElementById( hideBtnId ).addEventListener('click', () => {
-//         chrome.storage.local.set({'twitterTrendHiderFlag': 'HIDE'}, () => {
-//             chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-//                 let currentTab = tabs[0];
-//                 hideTwitterTrend(currentTab);
-//             });
-//         });
-//     });
-// }
-
-// if (document.getElementById( showBtnId )){
-//     document.getElementById( showBtnId ).addEventListener('click', () => {
-//         chrome.storage.local.set({'twitterTrendHiderFlag': 'SHOW'}, () => {
-//             chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-//                 let currentTab = tabs[0];
-
-//             });
-//         });
-//     });
-// }
-
-
-
-const hideTwitterTrend = async(tab) => {
-    chrome.tabs.executeScript(tab.id, {file: 'jquery-3.6.0.min.js'}, () => {
-        chrome.tabs.executeScript(tab.id, {file: 'hideTrend.js'}, () => {
-        });
-    });
-}
-
-
-
-// if (document.getElementById( hideBtnId )){
-//     document.getElementById( hideBtnId ).addEventListener('click', () => {
-        
-//     });
-// }
